3162704cee0a8af0d765676cf9a6e0312680bdfe
Tom <t4canty@gmail.com>
2020-02-10 11:27:30 -0800

    Began file handling.

--- src/src/Node.java
+++ src/src/Node.java
@@ -1,12 +1,24 @@
 package src;
 
+import java.awt.Graphics;
+import java.io.FileInputStream;
+
+import javax.swing.JButton;
+
 public class Node {
 	private int id; // id of the textfile
 	private String[] data; //send output of file to here.
 	private Node nextNode;
+	private FileInputStream fStream;
 	private int txtId; // gets the id from the textFile to point to the next object.
+	private JButton[] buttonArray;
 	public Node(int id) {
 		this.id = id;
+	}
+	private FileInputStream readFile() {return null;}
+	
+	public void paint(Graphics g, int x, int y) {
+		
 	}
 	
 }
