07ff36dc8386f359a22c7f4a061997ed77b2ac04
Tom <t4canty@gmail.com>
2020-02-10 11:17:54 -0800

    Created varibles neccessary for nodes.

--- src/src/Node.java
+++ src/src/Node.java
@@ -1,8 +1,12 @@
 package src;
 
 public class Node {
-	private int id;
+	private int id; // id of the textfile
 	private String[] data; //send output of file to here.
 	private Node nextNode;
-	private int[] idArr;
+	private int txtId; // gets the id from the textFile to point to the next object.
+	public Node(int id) {
+		this.id = id;
+	}
+	
 }
