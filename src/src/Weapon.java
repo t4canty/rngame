4440881c3e3a2b3c1b7e824a810108232db445ee
Tom <t4canty@gmail.com>
2020-02-10 11:18:47 -0800

    Added variables

--- src/src/Weapon.java
+++ src/src/Weapon.java
@@ -1,5 +1,24 @@
 package src;
 
-public enum Weapon {
-
+public class Weapon {
+	private String name;
+	private int damage;
+	private int value;
+	public Weapon(String name, int damage, int value) {
+		this.name = name;
+		this.damage = damage;
+		this.value = value;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public int getValue() {
+		return value;
+	}
 }
