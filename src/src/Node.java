b23258f90d15fa3858729524ba4288c6486a38d9
Tom <t4canty@gmail.com>
2020-02-10 20:09:00 -0800

    Began adding JSON support.

--- src/src/Node.java
+++ src/src/Node.java
@@ -2,20 +2,40 @@ package src;
 
 import java.awt.Graphics;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JButton;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 public class Node {
 	private int id; // id of the textfile
-	private String[] data; //send output of file to here.
+	private String data; //send output of file to here.
 	private Node nextNode;
-	private FileInputStream fStream;
+	private JSONObject j;
 	private int txtId; // gets the id from the textFile to point to the next object.
-	private JButton[] buttonArray;
-	public Node(int id) {
+	private ArrayList<JButton> buttonArray;
+	public Node(int id) throws FileNotFoundException, IOException, ParseException {
+		j = (JSONObject) new JSONParser().parse(new FileReader(id + ".json"));
+		String data = (String) j.get("text");
+		nextNode = new Node((int) j.get("nextNodeID"));
+		JSONArray ja = (JSONArray) j.get("buttons");
+		for(int i = 0; i < ja.size(); i++) {
+			JSONObject j2 = (JSONObject) ja.get(i);
+			buttonArray.add(new JButton((String) j2.get("Choice")));
+		}
 		this.id = id;
 	}
-	private FileInputStream readFile() {return null;}
+	private FileInputStream readFile() { 
+
+		return null;
+		}
 	
 	public void paint(Graphics g, int x, int y) {
 		
